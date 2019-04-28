eaba10b75c83d32bc4ad24bde073f6661cebcc35
tetel <tetel.player@gmail.com>
2019-04-28 08:41:58 -0300

    add atributes.pas

---
+++ class/attributes.pas
@@ -0,0 +1,48 @@
+unit attributes;
+
+interface
+
+type
+
+  TTable = class(TCustomAttribute)
+  private
+    FNameTable : string;
+  public
+    constructor Create(aNameTable: string);
+    property NameTable: string read FNameTable write FNameTable;
+  end;
+
+  TField = class(TCustomAttribute)
+  public
+    function IsPk: Boolean; virtual;
+  end;
+
+  TFieldPK = class(TField)
+  public
+    function IsPk: Boolean; override;
+  end;
+
+implementation
+
+{ TTable }
+
+constructor TTable.Create(aNameTable: string);
+begin
+  FNameTable := aNameTable;
+end;
+
+{ TFields }
+
+function TField.IsPk: Boolean;
+begin
+  Result := False;
+end;
+
+{ TFieldsPK }
+
+function TFieldPK.IsPk: Boolean;
+begin
+  Result := True;
+end;
+
+end.
